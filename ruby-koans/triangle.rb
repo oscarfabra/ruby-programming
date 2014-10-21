a37eeb0e630474b8a2e327e75a002d3edccc94ef
Oscar Fabra <oscarfabra@gmail.com>
2014-10-20 19:28:36 -0500

    triangle.rb done.

--- ruby-koans/triangle.rb
+++ ruby-koans/triangle.rb
@@ -14,6 +14,27 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+	begin
+		if a == 0 || b == 0 || c == 0
+			raise TriangleError, "All sides must be greater than 0."
+		elsif a < 0 || b < 0 || c < 0
+			raise TriangleError, "Sides cannot be negative."
+		else
+			unless a == b && b == c
+				# Finds biggest of sides
+				h = [a, b, c].max
+				if (h == a && (b + c <= a)) || (h == b && (a + c <= b)) ||
+						(h == c && (a + b <= c))
+					raise TriangleError, 
+					"Hypotenuse must be bigger than the sum of the two other sides."
+				end
+			end
+		end
+	rescue StandardError => ex
+		# Throws exception to method
+		raise ex.class, ex.message
+	end
+	# Proceeds with valid values
   if a == b && b == c
   	:equilateral
   elsif a == b || b == c || a == c
